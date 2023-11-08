7db292e7d0211f9a5b856df34550349f18f848b9
Andrew Milne <andrew@milne.me.uk>
2023-11-08 23:39:49 +0000

    2023-11-08 23:37
    - Fixed looping not working correctly
    - Corrected error in getPencilY formula, where a + was present instead of a -
    - Fixed text alignment issues
    - Altered window size and slider limits
    - Added error box for when inputs would not be possible in practice

--- src/Canvas.java
+++ src/Canvas.java
@@ -2,24 +2,28 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Canvas extends JPanel {
-    private static double t = 0;
+    private double t = 0;
     public Graphics myGraphics;
+    private int totalLoops;
     
     public void paintComponents(int bigR, int littleR, int o, int red, int green, int blue, int loops) {
         myGraphics = this.getGraphics();
         myGraphics.setColor(new Color((float)red/255,(float)green/255,(float)blue/255));
-        while(loops > 0) {
-            while(getT() < 2*Math.PI) {
+        int loopsComplete = totalLoops;
+        totalLoops += loops;
+        while(totalLoops > loopsComplete) {
+            loopsComplete++;
+            while(getT() < 2*Math.PI*loopsComplete) {
                 drawPoint(littleR, bigR, o);
             }
-            loops--;
         }
     }
     
     public void clear() {
         myGraphics = this.getGraphics();
         myGraphics.setColor(Color.WHITE);
-        myGraphics.fillRect(0,0,600,300);
+        myGraphics.fillRect(0,0,1200,900);
+        setT(0);
     }
     
     public void drawPoint(int littleR, int bigR, int o) {
@@ -32,18 +36,18 @@ public class Canvas extends JPanel {
     }
     
     public double getPencilX(int bigR, int littleR, int o) {
-        return (bigR-littleR)*Math.cos(getT()) + o*Math.cos(((double) (bigR - littleR) /littleR)*getT()) + 400;
+        return ((bigR-littleR)*Math.cos(getT()) + o*Math.cos(((double) (bigR - littleR) /littleR)*getT())) + 500;
     }
 
     public double getPencilY(int bigR, int littleR, int o) {
-        return (bigR-littleR)*Math.sin(getT()) + o*Math.sin(((double) (bigR - littleR) /littleR)*getT()) + 175;
+        return ((bigR-littleR)*Math.sin(getT()) - o*Math.sin(((double) (bigR - littleR) /littleR)*getT())) + 400;
     }
 
-    public static double getT() {
+    public double getT() {
         return t;
     }
 
-    public static void setT(double t) {
-        Canvas.t = t;
+    public void setT(double t) {
+        this.t = t;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,7 @@ public class Main {
     
     public void go() {
         JFrame frame = new JFrame();
-        JPanel panel = new JPanel();
+        Container panel = frame.getContentPane();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
         HashMap<Boolean,String> fonts = new HashMap<>();
         boolean renogareDownloaded = false;
@@ -30,15 +30,15 @@ public class Main {
         }
         panel.add(addLabel("Shape:", fonts.get(renogareDownloaded)));
         panel.add(addLabel("Fixed Radius"));
-        JSlider bigR = addSlider(0, 160, 10, 20);
+        JSlider bigR = addSlider(0, 400, 20, 40);
         panel.add(bigR);
 
         panel.add(addLabel("Moving Radius"));
-        JSlider littleR = addSlider(0, 80, 5, 10);
+        JSlider littleR = addSlider(0, 200, 10, 20);
         panel.add(littleR);
 
         panel.add(addLabel("Offset"));
-        JSlider o = addSlider(0, 25, 1, 5);
+        JSlider o = addSlider(0, 200, 10, 20);
         panel.add(o);
 
         panel.add(addLabel("Loops"));
@@ -65,7 +65,7 @@ public class Main {
 
         panel.add(Box.createVerticalStrut(10));
         JButton reset = new JButton("Reset");
-        goButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        reset.setAlignmentX(Component.CENTER_ALIGNMENT);
         panel.add(reset);
         
         panel.add(Box.createVerticalStrut(10));
@@ -74,9 +74,8 @@ public class Main {
         Canvas canvas = new Canvas();
         canvas.setBackground(Color.WHITE);
         panel.add(canvas);
-        frame.add(panel);
         
-        frame.setSize(800, 900);
+        frame.setSize(1000, 1400);
         frame.setVisible(true);
         
         frame.addWindowListener(new WindowAdapter() {
@@ -85,8 +84,16 @@ public class Main {
                 System.exit(0);
             }
         });
-        goButton.addActionListener(e -> canvas.paintComponents(bigR.getValue(), littleR.getValue(), o.getValue(), red.getValue(), green.getValue(), blue.getValue(), loops.getValue()));
+        goButton.addActionListener(e -> paintComponents(bigR.getValue(), littleR.getValue(), o.getValue(), red.getValue(), green.getValue(), blue.getValue(), loops.getValue(), canvas));
         reset.addActionListener(e -> canvas.clear());
+    }
+    
+    private void paintComponents(int bigR, int littleR, int o, int red, int green, int blue, int loops, Canvas canvas) {
+        if(bigR >= littleR && littleR >= o) {
+            canvas.paintComponents(bigR, littleR, o, red, green, blue, loops);
+        } else {
+            JOptionPane.showMessageDialog(null, "The fixed radius must be greater than the moving radius, which must be greater than the offset.","Error!",JOptionPane.ERROR_MESSAGE);
+        }
     }
     
     public JSlider addSlider(int min, int max, int space, int labelSpace) {
@@ -116,6 +123,7 @@ public class Main {
 
     public JLabel addLabel(String text, String font) {
         JLabel label = addLabel(text);
+        label.setAlignmentX(JLabel.CENTER_ALIGNMENT);
         label.setFont(new Font(font, Font.PLAIN, 20));
         label.setBackground(Color.WHITE);
         return label;
